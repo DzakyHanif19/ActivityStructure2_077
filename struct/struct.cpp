193ba28ca453b10f03e2f29cedc8a04964bf6cfb
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-04-05 11:04:41 +0700

    detail mahasiswa

--- struct/struct.cpp
+++ struct/struct.cpp
@@ -8,3 +8,33 @@ struct Mahasiswa {
 	string alamat;
 	int umur;
 };
+
+int main()
+{
+	Mahasiswa mhs1, mhs2;
+
+	mhs1.nim = "20210140077";
+	mhs1.nama = "Dzaky";
+	mhs1.alamat = "Bekasi";
+	mhs1.umur = 19;
+
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur;
+
+	cout << "\nNim : " << mhs1.nim;
+	cout << "\nnama : " << mhs1.nama;
+	cout << "\nalamat : " << mhs1.alamat;
+	cout << "\numur : " << mhs1.umur;
+
+	cout << "\nNim : " << mhs2.nim;
+	cout << "\nnama : " << mhs2.nama;
+	cout << "\nalamat : " << mhs2.alamat;
+	cout << "\numur : " << mhs2.umur;
+
+}
