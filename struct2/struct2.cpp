051030ad10cf6c1e88dcabd6663af376cef7e7db
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-04-05 11:21:22 +0700

    getline mahasiswa

--- struct2/struct2.cpp
+++ struct2/struct2.cpp
@@ -13,3 +13,28 @@ struct Mahasiswa {
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+
+	Mahasiswa mhs;
+
+		cout << "Masukkan nim: ";
+		cin.getline(mhs.nim, 12);
+		cout << "Masukan umur: ";
+		cin >> mhs.umur;
+		cin.ignore(1, '\n');
+		cout << "Masukkan nama: ";
+		cin.getline(mhs.nama, 20);
+		cout << "Alamat: ";
+		cout << "\tMasukan desa : ";
+		cin.getline(mhs.alamat.desa, 20);
+		cout << "\tMasukan kota : ";
+		cin.getline(mhs.alamat.kota, 20);
+		
+		cout << "\nNim : " << mhs.nim;
+		cout << "\nnama : " << mhs.nama;
+		cout << "\ndesa : " << mhs.alamat.desa;
+		cout << "\nkota : " << mhs.alamat.kota;
+		cout << "\numur : " << mhs.umur;
+
+}
